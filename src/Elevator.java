4ad1504bd87b3cc4047f228c50e28707be317128
Elijah Smith <smith.elijah97@gmail.com>
2019-07-16 12:34:51 -0400

    I started the constructor.. changes may come later

---
+++ src/Elevator.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */ 
+
+public class Elevator {
+    private int currentFloor;
+    private int destinationFloor;
+    private int numberOfFloors;
+
+    public Elevator(int currentFloor, int destinationFloor, int numberOfFloors) {
+        this.currentFloor = currentFloor;
+        this.destinationFloor = destinationFloor;
+        this.numberOfFloors = numberOfFloors;
+    }
+}
