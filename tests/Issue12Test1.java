4dc4a67c2d65145b0f939f383f274000cf857dc1
Will <wpjack1@gmail.com>
2019-07-17 11:04:46 -0400

    Add initial implementation of Passenger class.

---
+++ tests/Issue12Test1.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+public class Issue12Test1
+{
+   @Test
+   public void testPassengerConstructor1()
+   {
+      Passenger pass = new Passenger(3, 10);
+      
+      assertEquals(3, pass.getStartFloor());
+      assertEquals(10, pass.getDestinatonFloor());
+   }
+
+   @Test
+   public void testPassengerConstructor2()
+   {
+      Passenger pass = new Passenger(50);
+
+      assertEquals(1, pass.getStartFloor());
+      assertEquals(50, pass.getDestinationFloor());
+   }
+}
