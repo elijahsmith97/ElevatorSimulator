04e64a7f9978b547edc4266271677eabeb278472
Erik Burr <rkburr@gmail.com>
2019-07-17 10:30:23 -0400

    added move method that calls moveUp and moveDoen

--- Elevator.java
+++ Elevator.java
@@ -1,3 +1,18 @@
 public class Elevator {
 
+    /**
+     * Author: Erik Burr
+     * Issue #5 move()
+     */
+    public void move()
+    	{ System.out.println("Current floor: " + currentFloor);  
+    	if (destinationFloor > currentFloor) {
+      	moveUp(); 
+    	}
+    	else {
+      	moveDown();
+    	}
+    }
+
+
 }
