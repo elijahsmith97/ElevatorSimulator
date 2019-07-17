09270c777c479485e423b92a8b9d7bf9ba598233
Elijah Smith <smith.elijah97@gmail.com>
2019-07-16 23:35:59 -0400

    finsihed up on issue9 essentially the same way I did issue16 for moveDown().

--- Elevator.java
+++ Elevator.java
@@ -1,3 +1,12 @@
 public class Elevator {
-
+    /**
+     * Author: Elijah Smith
+     * Issue #9 moveUp() method
+     */ 
+    public void moveUp() {
+        int newCurrent = this.destinationFloor - this.currentFloor;
+        for(int i = 0; i < newCurrent; i++) {
+            this.currentFloor++;
+        }
+    }
 }
