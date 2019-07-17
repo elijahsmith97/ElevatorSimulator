2e53714b63047735396e8c21dd8e63178dbe9e2c
Elijah Smith <smith.elijah97@gmail.com>
2019-07-16 20:05:16 -0400

    Issue 16, 18 and 22 completed

--- src/Elevator.java
+++ src/Elevator.java
@@ -6,10 +6,32 @@ public class Elevator {
     private int currentFloor;
     private int destinationFloor;
     private int numberOfFloors;
-
+    
+    /**
+     * Author: Elijah Smith
+     * Issue #18 Constructor
+     */ 
     public Elevator(int currentFloor, int destinationFloor, int numberOfFloors) {
         this.currentFloor = currentFloor;
         this.destinationFloor = destinationFloor;
         this.numberOfFloors = numberOfFloors;
     }
+
+    /**
+     * Author: Elijah Smith
+     * Issue #16 moveDown()
+     */
+    public void moveDown(int destinationFloor) {
+        this.currentFloor = destinationFloor;
+    }
+
+    /**
+     * Author: Elijah Smith
+     * Issue #22 getter method
+     */ 
+    public int getCurrentFloor() {
+        return this.currentFloor;
+    }
 }
+
+
