a91a7c341694299c67273ebfd6905461ccbd8823
Erik A Burr <42555634+Erikaburr@users.noreply.github.com>
2019-07-18 11:52:09 -0400

    Update Elevator.java

--- Elevator.java
+++ Elevator.java
@@ -30,15 +30,17 @@ public class Elevator {
         }
     }
     
-      /*
-       *Author: Erik Burr
-       * Issue #9 moveUp
-       */
-     public void moveUp(){
+   /**
+    * Author: Erik Burr
+    * Issue #9 moveUp
+    */
+    public void moveUp(){
         int newCurrent = this.currentFloor - this.destinationFloor;
         for(int i = 0; i < newCurrent; i++) {
             while(this.currentFloor != this.destinationFloor) {
-            this.currentFloor--;}  
+            this.currentFloor--;
+         }
+     }
 
     /**
      * Author: Elijah Smith
@@ -46,16 +48,15 @@ public class Elevator {
      */ 
     public int getCurrentFloor() {
         return this.currentFloor;
-    }
-
-            
+     }
+	    
      /**
-      *Author: Erik Burr
+      * Author: Erik Burr
       * Issue #9 moveUp
       */
-	public int getDestinationFloor() {
-		return this.destinationFloor;
-	}
+     public int getDestinationFloor() {
+	  return this.destinationFloor;
+      }
 }
 
 
