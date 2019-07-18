7efeed1ad4ebf8e4858a0e8c0d70e83abad72386
Erik A Burr <42555634+Erikaburr@users.noreply.github.com>
2019-07-18 12:27:14 -0400

    Update ElevatorTest.java

--- ElevatorTest.java
+++ ElevatorTest.java
@@ -7,24 +7,38 @@ import org.junit.jupiter.api.Test;
 public class  ElevatorTest {
     /**
      * Author: ES
-     */ 
-    @Test
-    public void testEvaluate() {
-        Elevator elevator = new Elevator(5, 1, 5);
-        elevator.moveDown();
-        int newCurrent = elevator.getCurrentFloor();
-        assertEquals(1, newCurrent);
-    }
+     * Test method for moveDown
+     */
+	@Test
+	public void testEvaluateMoveDown() {
+        	Elevator elevator = new Elevator(5, 1, 5);
+        	elevator.moveDown();
+        	int newCurrent = elevator.getCurrentFloor();
+        	assertEquals(1, newCurrent);
+    	}
     
     /*
     * Author: Erik Burr
-    * Test method for move up
+    * Test method for moveUp
     */
-    
 	@Test
-	public void testEvaluate() {
-	  Elevator elev = new Elevator(1,5,5);
+	public void testEvaluateMoveUp() {
+		Elevator elev = new Elevator(1,5,5);
 		elev.moveUp();
 		int newCurrent = elev.getCurrentFloor();
-		assertEquals(1, newCurrent);	}
+		assertEquals(1, newCurrent);	
+	}
+	
+    /*
+    * Author: Erik Burr
+    * Test method for floorDifference
+    */
+	@Test
+	public void testEvaluateFloorDifference() {
+		Elevator elev = new Elevator(1,5,5);
+		elev.floorDiffference(3, 5);
+		int newCurrent = elev.getCurrentFloor();
+		assertEquals(2, newCurrent);	
+	}
+	
 }
