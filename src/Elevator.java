443721ca5b689af8b7fb5745d445999d81bdb4cb
Erik A Burr <42555634+Erikaburr@users.noreply.github.com>
2019-07-22 10:59:33 -0400

    Update Elevator.java

--- src/Elevator.java
+++ src/Elevator.java
@@ -34,11 +34,10 @@ public class Elevator {
     * Issue #9 moveUp
     */
     public void moveUp(){
-        int newCurrent = this.currentFloor - this.destinationFloor;
+        int newCurrent = this.destinationFloor - this.currentFloor;
         for(int i = 0; i < newCurrent; i++) {
-            while(this.currentFloor != this.destinationFloor) {
-            this.currentFloor--;
-         }
+            this.currentFloor++;
+        }
      }
 
     /**
