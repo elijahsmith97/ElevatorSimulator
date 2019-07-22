3701b61eb93a3cf3d9ef8134c34332dbcf8dbcc7
Jake Wooten <wootendj@appstate.edu>
2019-07-22 11:02:12 -0400

    error with objects in display fixed

--- src/ElevSim.java
+++ src/ElevSim.java
@@ -10,7 +10,7 @@ public class ElevSim {
          //creating the objects
          Elevator elev = new Elevator(elev.getCurrentFloor(), elev.getDestinationFloor(), elev.getnumberOfFloors());
          Passenger pass = new Passenger(pass.getDestinationFloor());
-         Display status = new Display(Passenger pass, Elevator elev);
+         Display status = new Display(pass, elev);
          
          //displaying current floor or pasenger and elevator
          status.display_currentP();
