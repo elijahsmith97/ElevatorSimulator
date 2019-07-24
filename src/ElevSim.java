5ef8d34ef8b3bf2c287ed4b4b67ea27256420291
Erik A Burr <42555634+Erikaburr@users.noreply.github.com>
2019-07-24 10:29:13 -0400

    Update ElevSim.java

--- src/ElevSim.java
+++ src/ElevSim.java
@@ -3,9 +3,15 @@
  * version: Mon Jul 15, 2019
  * Issue #2
  */
- 
+import java.util.Scanner; 
+
 public class ElevSim {
     public static void main(String[] args) {
-    
+         Scanner kb = new Scanner(System.in);
+         System.out.println("Enter the desired floor: ");
+         int df = kb.nextInt(); //df is desired floor
+
+         //hard coded 5 as the numberOfFloor parameter because Im not quite sure how to handle that now
+         Elevator elevator = new Elevator(1, df, 5);
     }
 }
