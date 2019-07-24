bade8f0e58e8f89c02fd9a0765c8dc0e3e02f993
Will <wpjack1@gmail.com>
2019-07-24 12:04:42 -0400

    Started the DefaultControllers next() method.

--- src/DefaultController.java
+++ src/DefaultController.java
@@ -1,12 +1,64 @@
+import java.util.ArrayList;
+
 public class DefaultController extends Controller
 {
+   private bool movingUp;
+   private bool movingDown;
+   
    public DefaultController(Elevator elevator)
    {
       super(elevator);
+
+      this.movingUp = false;
+      this.movingDown = false;
    }
 
    public void next()
    {
+      int maxFloor = elevator.//getMaxfloor
+      int currentFloor = elevator.//getcurrentfloor
+
+      ArrayList<int> destFloors = elevator.//getDestFloors
+      boolean pushUpFloors[] = elevator.//getPushUpFloors
+      boolean pushDownFloors[] = elevator.//getPushDownFloors
+
+
+      if(currentFloor == 0 || currentFloor == maxFloor)
+      {
+         movingUp = false;
+         movingDown = false;
+      }
+
+      if(currentFloor == 0 && (destFloors.size() > 0 || elevator.//pushTrue))
+      {
+         movingUp = true;
+      }
+      else if (currentFloor == maxFloor && (destFloors.size() > 0 || elevator.//pushTrue))
+      {
+         movingDown = true;
+      }
+
+      if(destFloors.contains(currentFloor))
+      {
+         elevator.stop();
+      }
+      else if(movingUp && pushUpFloors[currentFloor])
+      {
+         elevator.stop();
+      }
+      else if(movingDown && pushDownFloors[currentFloor])
+      {
+         elevator.stop();
+      }
+
+      if(movingUp)
+      {
+         elevator.moveUp();
+      }
+      else if(movingDown)
+      {
+         elevator.moveDown();
+      }
 
    }
 }
