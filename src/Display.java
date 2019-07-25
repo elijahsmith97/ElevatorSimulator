5afa11754ebe6c6b33450dec1bd45bd13300e7dc
Erik A Burr <42555634+Erikaburr@users.noreply.github.com>
2019-07-25 11:55:50 -0400

    Update Display.java

--- src/Display.java
+++ src/Display.java
@@ -1,43 +1,44 @@
 /*
- *
- * Author: Matthew Frankel
- * Issue 56
- * */
+*
+* Author: Matthew Frankel
+* Issue 56
+* */
 
 public class Display 
 {
-    Elevator el;
-    PassengerManager pm;
+   Elevator el;
+   PassengerManager pm;
 
-    public Display(PassengerManager pm, Elevator el) 
-    {   
-        this.pm = pm;
-        this.el = el;
-    }
+   public Display(PassengerManager pm, Elevator el) 
+   {   
+       this.pm = pm;
+       this.el = el;
+   }
+   
+   public void start()
+   {
+       System.out.println("Starting simulation...");
+       System.out.println("Press \"s\" to exit at any time");
+   }
+   
+   public void goingUp()
+   {
+       System.out.println("Elevator going up...");
+       el.getCurrentFloor();
+   }
+   
+   public void goingDown()
+   {
+       System.out.println("Elevator going down...");                   
+   }
     
-    public void start()
-    {
-        System.out.println("Starting simulation...");
-        System.out.println("Press \"s\" to exit at any time");
-    }
-    
-    public void goingUp()
-    {
-        System.out.println("Elevator going up...");                
-    }
-    
-    public void goingDown()
-    {
-        System.out.println("Elevator going down...");                   
-    }
-     
-    public void exit()
-    {
-        System.out.println("Exiting simulation...");                   
-    }
-                           
-    public void displayCurrentFloor()
-    {
-        System.out.println("Elevator is currently on floor " + el.getCurrentFloor());
-    }
+   public void exit()
+   {
+       System.out.println("Exiting simulation...");                   
+   }
+                          
+   public void displayCurrentFloor()
+   {
+       System.out.println("Elevator is currently on floor " + el.getCurrentFloor());
+   }
 }
