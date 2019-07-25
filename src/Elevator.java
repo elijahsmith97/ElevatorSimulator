26393523b98f77cbbbdcbf203b57a73aa8111312
Erik A Burr <42555634+Erikaburr@users.noreply.github.com>
2019-07-25 11:38:37 -0400

    Update Elevator.java

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,13 +1,24 @@
-/**
- * 
- */ 
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class Elevator {
 
     private int currentFloor;
     private int destinationFloor;
     private int numberOfFloors;
-    
+    private boolean[] pushedUp;
+    private boolean[] pushedDown;
+    private boolean stopped;
+
+    /**
+     * Author: Elijah Smith
+     * Issue #61 new Constructor
+     */ 
+    public Elevator(int numberOfFloors) {
+        this.currentFloor = 1;
+        pushedUp = new boolean[numberOfFloors + 1];
+        pushedDown = new boolean[numberOfFloors + 1];
+    }
     /**
      * Author: Elijah Smith
      * Issue #18 Constructor
@@ -16,38 +27,79 @@ public class Elevator {
         this.currentFloor = currentFloor;
         this.destinationFloor = destinationFloor;
         this.numberOfFloors = numberOfFloors;
+        pushedUp = new boolean[numberOfFloors];
+        pushedDown = new boolean[numberOfFloors];
     }
 
     /**
      * Author: Elijah Smith
      * Issue #57/51 moveDown()
+     * @throws Exception 
      */
     public void moveDown() {
         currentFloor--;
+        stopped = false;
+        try {
+            Thread.sleep(3000);
+            System.out.println("Elevator going down...");
+        } 
+        
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
     
    /**
     * Author: Elijah Smith
     * Issue #57/51 moveUp
     */
-    public void moveUp(){
-        currentFloor++;        
+    public void moveUp() {
+        currentFloor++;
+        stopped = false;
+        try {
+            Thread.sleep(3000);
+            System.out.println("Elevator going up...");
+        } 
+        
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-
+    
+    /**
+    * Author: Erik Burr
+    * Issue #17 pushUp/pushDown
+    */
+    public void pushUp(int currentFloor){
+        pushedUp[currentFloor] = true;   
+    }
+             
+    public void pushDown(int currentFloor){
+        pushedDown[currentFloor] = true;  
+    }
+    
     /**
      * Author: Elijah Smith
      * Issue #22 getter method
      */ 
-    public int getCurrentFloor() {
+      public int getCurrentFloor() {
         return this.currentFloor;
-     }
-	    
-     public int getDestinationFloor() {
-	  return this.destinationFloor;
       }
-
+  
+      public int getDestinationFloor() {
+      return this.destinationFloor;
+      }
+      
       public int getNumberOfFloors() {
         return this.numberOfFloors;
+      }
+
+      public void stop() {
+        this.stopped = true;
+      }
+
+      public boolean getStopped() {
+        return this.stopped;
       }
 
       public void setCurrentFloor(int currentFloor) {
@@ -62,5 +114,3 @@ public class Elevator {
         this.numberOfFloors = numberOfFloors;
       }
 }
-
-
