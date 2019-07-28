d590884dce21a330220e96af297df92a3aeaa91f
Erik A Burr <42555634+Erikaburr@users.noreply.github.com>
2019-07-28 13:33:33 -0400

    Update ElevSim.java

--- src/ElevSim.java
+++ src/ElevSim.java
@@ -1,39 +1,51 @@
 import java.util.Scanner; 
 
 public class ElevSim {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner kb = new Scanner(System.in);
         int elevCurrent = 1;
         int passCurrent = 1;
         char decision = 'u';
         int maxFloors = 100;
-        //Elevator elev = new Elevator(maxFloors);
+        Elevator elev = new Elevator(maxFloors);
+        Timer time = new Timer(3000);
+        int current = elev.getCurrentFloor();
         //Controller control = new Controller(elev);
-
+        System.out.println("Welcome to our Elevator Simulation \n\n\n");
+        
+        
         while(Character.toLowerCase(decision) != 's') {
-            System.out.print("Which direction would you like to go? "/* + elev.setCurrentFloor()*/);
+            
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+            System.out.println("The elevator has arrived: \nFloor Level " + current + "\n\n");
+            System.out.println("The doors are opening, please enter the elevator...");
+            
+            
+            
+            
+            System.out.print("Which direction would you like to go? \n Please enter: \n u for up, \n d for down."/* + elev.setCurrentFloor()*/);
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            
+            
+            
             decision = kb.next().charAt(0);
             switch(Character.toLowerCase(decision)) {
                 case 'u':
-                    try {
-                        Thread.sleep(3000);
-                        System.out.println("Going up...");
-                    } 
-                    
-                    catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                System.out.println("Going up to floor " + (current + 1));
+                time.run();
+                current++;
                     break;
                 
                 case 'd':
-                    try {
-                        Thread.sleep(3000);
-                        System.out.println("Going down...");
-                    } 
-                    
-                    catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }    
+                    System.out.println("Going down to floor " + (current - 1));
+                    time.run();
+                    current--;   
                     break;
                 
                 default:
@@ -53,7 +65,8 @@ public class ElevSim {
             */
             char decision2 = 'y';
             while(Character.toLowerCase(decision2) != 'n' && decision != 's') {
-                System.out.print("Are there new passengers? ");
+                System.out.println("\nAre there new passengers? ");
+                System.out.println("\nPlease enter: \nn for No, \ny for Yes \n\n\n");
                 decision2 = kb.next().charAt(0);
                 switch(Character.toLowerCase(decision2)) {
                     case 'y':
@@ -62,7 +75,7 @@ public class ElevSim {
                         break;
                     case 'n':
                         decision2 = 'n';
-                        System.out.print("There are no new passengers, continuing simulation...\n"); 
+                        System.out.print("There are no new passengers, continuing simulation...\n\n"); 
                         break;
                     case 's':
                         decision = 's';
