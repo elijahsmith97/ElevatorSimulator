bdc6fb7d5de7880723e5d4dbe09202e0a5b7f81c
Erik A Burr <42555634+Erikaburr@users.noreply.github.com>
2019-07-28 15:01:18 -0400

    Number of floor, and passangeer
    Main method updated to keep track of floor and passenger, updated some of the display prompts

--- src/ElevSim.java
+++ src/ElevSim.java
@@ -10,29 +10,34 @@ public class ElevSim {
         Elevator elev = new Elevator(maxFloors);
         Timer time = new Timer(3000);
         Timer time2 = new Timer(2000);
-
         int current = elev.getCurrentFloor();
+        int passanger = 1;
         //Controller control = new Controller(elev);
-        System.out.println("Welcome to our Elevator Simulation \n\n\n");
+        System.out.println("Welcome to our Elevator Simulation:\n  (Enter s to exit simulation) \n\n\n");
+        time2.run();
+        System.out.println("The doors are opening...\n");
+        time2.run();
+        System.out.println("Please enter the elevator:");
+        time2.run();
+
+
         
         while(Character.toLowerCase(decision) != 's') {
-            time2.run();
-            System.out.println("The elevator has arrived: \nFloor Level " + current + "\n\n");
-            System.out.println("The doors are opening, please enter the elevator...");
-            System.out.print("Which direction would you like to go? \n Please enter: \n u for up, \n d for down. \n\n");
+
+            System.out.println("Which direction would you like to go? \n Please enter: \n u for up, \n d for down \n\n");
             decision = kb.next().charAt(0);
+            
             switch(Character.toLowerCase(decision)) {
                 case 'u':
-
-                System.out.println("\nGoing up to floor " + (current + 1));
+                System.out.println("\nGoing up to floor " + (current + 1) + "\n");
                 time.run();
                 current++;
                     break;
                 
                 case 'd':
-                System.out.println("\nGoing down to floor " + (current - 1));
-                time.run();
-                current--;
+                    System.out.println("\nGoing down to floor " + (current - 1) + "\n");
+                    time.run();
+                    current--;   
                     break;
                 
                 default:
@@ -41,29 +46,42 @@ public class ElevSim {
                     break;
             }
             
-            
             /* cycle for next passenger
             passManager.next();
             control.next();
             status.display();
             */
+
             /*if(display.getStatus() == 's')
                 decision = "s";
             */
             char decision2 = 'y';
+            System.out.println("The elevator has arrived: \nFloor Level: " + current + "\n\n");
+            
             while(Character.toLowerCase(decision2) != 'n' && decision != 's') {
-                System.out.println("\nAre there new passengers? ");
-                System.out.println(" Please enter: \n n for No, \n y for Yes \n");
+                System.out.println("\nAre there new passengers? \n Number of Passangers: " + passanger);
+                System.out.println(" Please enter: \n n for No, \n y for Yes, \n l to enter number of passangers that have left.\n");
                 decision2 = kb.next().charAt(0);
                 switch(Character.toLowerCase(decision2)) {
                     case 'y':
+                        passanger++;
                         //passManager.newPassenger(//whatever this entails);
-                        System.out.println("\nNew passenger added to list!\n");
+                        System.out.println("\nNew passenger added to list!");
                         break;
                     case 'n':
                         decision2 = 'n';
-                        System.out.println("There are no new passengers, continuing simulation...\n\n"); 
+                        System.out.println("There are no new passengers, doors closing...\n\n"); 
                         break;
+                    
+                    case 'l':
+                        decision2 = 'l';
+                        int departing;
+                        System.out.println("Enter number of passangers leaving:");
+                        departing = kb.nextInt();
+                        passanger = passanger - departing;
+                        System.out.println(departing + " passenger(s) have left, " + passanger + " passangers remain."); 
+                        break;
+                        
                     case 's':
                         decision = 's';
                         System.out.println("Exiting simulation...\n");
