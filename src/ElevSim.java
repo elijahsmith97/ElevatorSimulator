41ed3247f2ed1c87d21dff194a69e78dcfa81e6e
Erik A Burr <42555634+Erikaburr@users.noreply.github.com>
2019-07-30 12:28:23 -0400

    Update ElevSim.java

--- src/ElevSim.java
+++ src/ElevSim.java
@@ -1,22 +1,32 @@
+package solution;
+import java.util.Scanner; 
+
 public class ElevSim {
     
+
     public static void main(String[] args) throws InterruptedException {
+
         int maxFloors = 10;
-      
+        Scanner kb = new Scanner(System.in);
         Elevator elev = new Elevator(maxFloors);
         PassengerManager passengerManager= new PassengerManager(elev);
         Controller control = new DefaultController(elev);
       
-        System.out.println("Welcome to the Elevator Simulator.\n\nElevator is currently on " + elev.getCurrentFloor());
+        System.out.println("Welcome to the Elevator Simulator.\n\nElevator is currently on: " + elev.getCurrentFloor());
         
         Passenger p1 = passengerManager.newPassenger();
         Passenger p2 = passengerManager.newPassenger();
+        
         
         while(true)
         {
             passengerManager.next();
             control.next();
-            System.out.println("\nElevator is currently on " + elev.getCurrentFloor()); 
+            System.out.println("\nPassenger 1 is on floor " + p1.getDestinationFloor());
+            System.out.println("Passenger 2 is on floor " + p2.getDestinationFloor());
+            
+            System.out.println("\nElevator is currently on: " + elev.getCurrentFloor());
+            
         }
-    }
+   }
 }
