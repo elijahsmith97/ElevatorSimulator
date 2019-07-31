43dad440b4ea358eb2a1169a088541da1fba7c4a
Elijah Smith <smith.elijah97@gmail.com>
2019-07-31 11:19:29 -0400

    removing tests

--- tests/Tests.java
+++ tests/Tests.java
@@ -32,30 +32,7 @@ public class Tests implements TimerClient
         assertEquals(2, newCurrent);
    }
    
-      /**
-    * Author: Erik Burr
-    *
-    */
-   @Test
-   public void testPushUp() {
-        Elevator elev = new Elevator(1, 5);   
-        int newCurrent = elev.getCurrentFloor();
-        elev.pushUp(newCurrent);
-        assertEquals(1, newCurrent);   
-   }
-
-   /**
-    * Author: Erik Burr
-    *
-    */
-   @Test
-   public void testPushDown() {
-        Elevator elev = new Elevator(1, 5);   
-        int newCurrent = elev.getCurrentFloor();
-        elev.pushDown(newCurrent);
-        assertEquals(1, newCurrent);  
-   }
-   
+  
     //simple values needed for Timer
     double numSeconds = 4.5;
     double pollDelay = 0.25;
@@ -209,27 +186,8 @@ public class Tests implements TimerClient
 
         assertEquals(2, elev.getCurrentFloor());
         assertEquals(100, elev.getNumberOfFloors());
+
     }
-    /**
-     *this is a comment
-      */
-   @Test
-   public void testPushTrue1()
-   {
-      Elevator elev = new Elevator(1, 10);
-
-      assertEquals(false, elev.pushTrue());
-   }
-
-   @Test 
-   public void testPushTrue2()
-   {
-      Elevator elev = new Elevator(1, 10);
-      elev.pushUp(4);
-
-      assertEquals(true, elev.pushTrue());
-
-   }
 
    @Test
    public void testPassengerNext1()
@@ -241,22 +199,7 @@ public class Tests implements TimerClient
 
       assertEquals(true, elev.pushTrue());      
    }
-
-    public void testGetPushedUpArray() {
-        Elevator elev = new Elevator(100);
-        elev.pushUp(3);
-        elev.pushDown(4);
-        
-        boolean[] goingUp = elev.getPushedUpArray();
-        boolean[] goingDown = elev.getPushedDownArray();
-
-        boolean up = goingUp[3];
-        boolean down = goingDown[4];
-    
-        assertEquals(true, up);
-        assertEquals(true, down);
-    }
-   
+  
      @Test 
    public void testdisplay1()
    {
