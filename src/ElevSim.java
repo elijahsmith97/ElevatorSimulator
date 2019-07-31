66efa97aa74b2c72b6335f3540aa0fde0455eea1
Jake Wooten <wootendj@appstate.edu>
2019-07-31 11:47:14 -0400

    took out variables at the beginning of the class that were unneeded

--- src/ElevSim.java
+++ src/ElevSim.java
@@ -4,13 +4,6 @@ public class ElevSim {
     
     
     public static void main(String[] args) throws InterruptedException {
-        int elevCurrent = 1;
-        int passDestination = 1;
-        int passCurrent = 1;
-        char decision = 'u';
-        int floorDecision = 1;
-        String stringDecision = "";
-        int maxFloors = 10;
         Scanner kb = new Scanner(System.in);
       
         Elevator elev = new Elevator(maxFloors);
