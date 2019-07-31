b65624fe4088e3000bf271ac695853cd9930283a
frankelms <43014076+frankelms@users.noreply.github.com>
2019-07-31 11:05:20 -0400

    Changed constructor to take an array

--- src/Controller.java
+++ src/Controller.java
@@ -1,10 +1,8 @@
-import java.util.ArrayList;
-
 public abstract class Controller
 {
-   protected ArrayList<Elevator> elevators;
+   protected Elevator[] elevators;
 
-   protected Controller(ArrayList<Elevator> elevators)
+   protected Controller(Elevator[] elevators)
    {
       this.elevators = elevators;
    }
