d6d2f00e5eae60aa1c798099f5838fdf6d2bfc8e
Elijah Smith <smith.elijah97@gmail.com>
2019-08-01 10:40:54 -0400

    Changed back to old implementation within Controller

--- src/Controller.java
+++ src/Controller.java
@@ -16,7 +16,31 @@ public abstract class Controller
    }
 
    public abstract void next();
-   public abstract void pushUp(int currentFloor);
-   public abstract void pushDown(int currentFloor);
-   public abstract boolean pushTrue();
+          /**
+     * Editor: Elijah Smith
+     * Issue #108 moving this method from Elevator
+     */ 
+    public void pushUp(int currentFloor) {
+        pushedUp[currentFloor] = true;    
+    }
+
+    /**
+     * Editor: Elijah Smith
+     * Issue #108 moving this method from Elevator
+     */ 
+    public void pushDown(int currentFloor) {
+        pushedDown[currentFloor] = true;
+    }
+
+    /**
+     * Editor: Elijah Smith
+     * Issue #108 moving this method from Elevator
+     */ 
+    public boolean pushTrue() {
+        for(int loop = 0; loop < pushedUp.length; loop++) {
+            if(pushedUp[loop] || pushedDown[loop])
+                return true;
+        }
+        return false;
+    }
 }
