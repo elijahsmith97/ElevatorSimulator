187a3cc37b93a7740b060ec75646f8c459d6c1f2
Will <wpjack1@gmail.com>
2019-08-01 12:10:18 -0400

    Bug Fixes.

--- src/ElevSim.java
+++ src/ElevSim.java
@@ -5,16 +5,9 @@ public class ElevSim {
     
     public static void main(String[] args) throws InterruptedException {
       
-        System.out.println("Welcome to the Elevator Simulator.");
         Controller control = new DefaultController(10, 2);
         PassengerManager passengerManager = new PassengerManager(control); 
-        Passenger pass = new Passenger(elev, 0);
-        
-        System.out.println("Welcome to the Elevator Simulator.\n\nElevator is currently on: " + elev.getCurrentFloor());
-        System.out.println("\nPassenger 1, which floor would you like to go?");
-        int destination = kb.nextInt();
-        pass.setDestinationFloor(destination);
-        System.out.println("Passenger 1 is going to: " + destination);
+        Passenger pass = new Passenger(control, 0);
         
         Passenger p1 = passengerManager.newPassenger();
         Passenger p2 = passengerManager.newPassenger();
@@ -24,7 +17,6 @@ public class ElevSim {
         {
             passengerManager.next();
             control.next(); 
-            System.out.println("\nElevator is currently on: " + elev.getCurrentFloor());
         }
    }
 }
